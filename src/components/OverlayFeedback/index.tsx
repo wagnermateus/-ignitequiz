3b5363146ce590f6c164df0f7670c1a33737912b
wagnermateus <wagnermateus7@gmail.com>
2023-07-09 21:38:31 +0100

    Feedback Visual - Animando o OverlayFeedback

--- src/components/OverlayFeedback/index.tsx
+++ src/components/OverlayFeedback/index.tsx
@@ -1,7 +1,14 @@
 import { useWindowDimensions } from "react-native";
-import Animated from "react-native-reanimated";
+import Animated, {
+  Easing,
+  useAnimatedStyle,
+  useSharedValue,
+  withSequence,
+  withTiming,
+} from "react-native-reanimated";
 import { Canvas, Rect, BlurMask } from "@shopify/react-native-skia";
 import { THEME } from "../../styles/theme";
+import { useEffect } from "react";
 
 const STATUS = [
   "transparent",
@@ -14,12 +21,28 @@ type Props = {
 };
 
 export function OverlayFeedback({ status }: Props) {
+  const opactity = useSharedValue(0);
   const { height, width } = useWindowDimensions();
+
+  const styleAnimated = useAnimatedStyle(() => {
+    return {
+      opacity: opactity.value,
+    };
+  });
 
   const color = STATUS[status];
 
+  useEffect(() => {
+    opactity.value = withSequence(
+      withTiming(1, { duration: 400, easing: Easing.bounce }),
+      withTiming(0)
+    );
+  }, [status]);
+
   return (
-    <Animated.View style={{ width, height, position: "absolute" }}>
+    <Animated.View
+      style={[{ width, height, position: "absolute" }, styleAnimated]}
+    >
       <Canvas style={{ flex: 1 }}>
         <Rect x={0} y={0} width={width} height={height} color={color}>
           <BlurMask blur={50} style="inner" />
--- src/screens/Quiz/index.tsx
+++ src/screens/Quiz/index.tsx
@@ -28,7 +28,7 @@ import { ConfirmButton } from "../../components/ConfirmButton";
 import { OutlineButton } from "../../components/OutlineButton";
 import { THEME } from "../../styles/theme";
 import { ProgressBar } from "../../components/ProgressBar";
-import { OverlayFeedback } from "../../components/OverlayFeedback/inde";
+import { OverlayFeedback } from "../../components/OverlayFeedback";
 
 interface Params {
   id: string;
@@ -40,6 +40,7 @@ const CARD_INCLINATION = 10;
 const CARD_SKIP_AREA = -200;
 
 export function Quiz() {
+  const [statusReply, setStatusReply] = useState(0);
   const [points, setPoints] = useState(0);
   const [isLoading, setIsLoading] = useState(true);
   const [currentQuestion, setCurrentQuestion] = useState(0);
@@ -93,8 +94,10 @@ export function Quiz() {
     }
 
     if (quiz.questions[currentQuestion].correct === alternativeSelected) {
+      setStatusReply(1);
       setPoints((prevState) => prevState + 1);
     } else {
+      setStatusReply(2);
       shakeAnimation();
     }
 
@@ -215,7 +218,7 @@ export function Quiz() {
 
   return (
     <View style={styles.container}>
-      <OverlayFeedback status={0} />
+      <OverlayFeedback status={statusReply} />
 
       <Animated.View style={fixedProgressBarStyles}>
         <Text style={styles.title}>{quiz.title}</Text>
